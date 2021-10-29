2acb5c87fb42d93323e8a9f160a7ded838e24716
Blake Hildebrand <baugusthildebrand@gmail.com>
2021-10-28 19:14:34 -0700

    Change to mock URL test

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,14 @@ pub mod response_types;
 
 use response_types::WarDataResponse;
 
+const WAR_DATA: &'static str = "/worldconquest/war";
+
 pub struct Client {
     web_client: reqwest::Client,
 }
 
 impl Client {
     pub async fn war_data(&self) -> WarDataResponse {
-        const WAR_DATA: &str = "/worldconquest/war";
-
         let request_string = build_request(WAR_DATA);
 
         let response = self.web_client.get(request_string).send().await.unwrap();
@@ -26,23 +26,52 @@ impl Default for Client {
 }
 
 fn build_request(endpoint: &'static str) -> String {
-    const API_URL: &str = "https://war-service-live.foxholeservices.com/api";
+    #[cfg(not(test))]
+    let mut request_string = "https://war-service-live.foxholeservices.com/api".to_string();
 
-    let mut request_string = String::from(API_URL);
+    #[cfg(test)]
+    let mut request_string = mockito::server_url();
+
     request_string.push_str(endpoint);
-
     request_string
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use mockito::mock;
 
     #[tokio::test]
     async fn test_war_data() {
+        let war_data_string = r#"{
+            "warId" : "1e82269a-d82b-4350-b1b1-06a98c983503",
+            "warNumber" : 83,
+            "winner" : "NONE",
+            "conquestStartTime" : 1632326703205,
+            "conquestEndTime" : null,
+            "resistanceStartTime" : null,
+            "requiredVictoryTowns" : 32
+        }"#;
+
+        let expected_response = WarDataResponse {
+            war_id: "1e82269a-d82b-4350-b1b1-06a98c983503".to_string(),
+            war_number: 83,
+            winner: "NONE".to_string(),
+            conquest_start_time: 1632326703205,
+            conquest_end_time: None,
+            resistance_start_time: None,
+            required_victory_towns: 32,
+        };
+
+        let _m = mock("GET", WAR_DATA)
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(war_data_string)
+            .create();
+
         let client = Client::default();
 
         let response = client.war_data().await;
-        println!("{:?}", response);
+        assert_eq!(expected_response, response);
     }
 }
--- src/response_types.rs
+++ src/response_types.rs
@@ -12,34 +12,3 @@ pub struct WarDataResponse {
     pub resistance_start_time: Option<u64>,
     pub required_victory_towns: u8,
 }
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_war_data_deserialize() {
-        let war_data_string = r#"{
-            "warId" : "1e82269a-d82b-4350-b1b1-06a98c983503",
-            "warNumber" : 83,
-            "winner" : "NONE",
-            "conquestStartTime" : 1632326703205,
-            "conquestEndTime" : null,
-            "resistanceStartTime" : null,
-            "requiredVictoryTowns" : 32
-        }"#;
-
-        let expected_response = WarDataResponse {
-            war_id: "1e82269a-d82b-4350-b1b1-06a98c983503".to_string(),
-            war_number: 83,
-            winner: "NONE".to_string(),
-            conquest_start_time: 1632326703205,
-            conquest_end_time: None,
-            resistance_start_time: None,
-            required_victory_towns: 32,
-        };
-
-        let response: WarDataResponse = serde_json::from_str(&war_data_string).unwrap();
-        assert_eq!(expected_response, response);
-    }
-}
